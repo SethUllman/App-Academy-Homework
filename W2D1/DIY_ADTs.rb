4f75d51fc8cbaf038b2a7cecda1d7a9b8d71a143
Seth Ullman <seth.ullman98@gmail.com>
2019-09-09 01:38:55 -0600

    added queue skeleton

--- W2D1/DIY_ADTs.rb
+++ W2D1/DIY_ADTs.rb
@@ -17,11 +17,37 @@ class Stack
   end
 end
 
-stack = Stack.new
-stack.push("string")
-p stack.stack
-stack.push(52)
-p stack.stack
-p stack.peek
-stack.pop
-p stack.stack
+class Queue
+  attr_reader :line
+
+  def initialize
+    @line = Array.new
+  end
+
+  def enqueue(ele)
+    @line << ele
+  end
+
+  def dequeue
+    @line.shift
+  end
+
+  def peek
+    @line[-1]
+  end
+end
+
+#queue = Queue.new
+#p queue.line
+#queue.enqueue("ele1")
+#p queue.line
+#queue.enqueue("ele2")
+#p queue.line
+#queue.enqueue("ele3")
+#p queue.line
+#queue.dequeue
+#p queue.line
+#queue.enqueue("ele4")
+#p queue.line
+#p queue.peek
+
