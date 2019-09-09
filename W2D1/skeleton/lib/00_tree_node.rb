91b8723249f087eb4ac318b2e9923013bc02c81a
Seth Ullman <seth.ullman98@gmail.com>
2019-09-09 15:19:20 -0600

    adds working node.parent= method

--- W2D1/skeleton/lib/00_tree_node.rb
+++ W2D1/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,30 @@
 class PolyTreeNode
 
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    if node.nil?
+      @parent = nil
+    else
+      @parent.children.delete(self) if !@parent.nil?
+      @parent = node
+      @parent.children << self if !@parent.children.include?(self) && !@parent.children.nil?
+    end 
+    
+  end
+
+  def inspect
+    @value.inspect
+  end
+
 end
+
+
+
+ 
