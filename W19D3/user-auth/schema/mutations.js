1d0c85419d1f0306af96ff2183a939a0f4106293
Seth Ullman <seth.ullman98@gmail.com>
2019-12-10 23:40:36 -0700

    add homework for W18D3

---
+++ W19D3/user-auth/schema/mutations.js
@@ -0,0 +1,27 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLNonNull } = graphql;
+const mongoose = require("mongoose");
+const UserType = require("./user_type");
+const validateInputs = require("../validation/register");
+const AuthService = require("../services/auth");
+
+const User = mongoose.model("user");
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    register: {
+      type: UserType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, data) {
+        return AuthService(data);
+      }
+    }
+  }
+});
+
+module.exports = mutation;
