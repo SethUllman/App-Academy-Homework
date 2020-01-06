912d2fbb466926e346440a468abafc5028c2f4f3
Seth Ullman <seth.ullman98@gmail.com>
2020-01-06 13:58:57 -0700

    add finished pow function

--- W22D1/recursion_project/lib/problems.js
+++ W22D1/recursion_project/lib/problems.js
@@ -82,7 +82,11 @@ function reverseString(str) {
 // pow(3, 4)    // => 81
 // pow(2, -5)   // => 0.03125
 function pow(base, exponent) {
+    if (exponent === 0) return 1;
 
+    if (exponent > 0) return base * pow(base, exponent-1);
+
+    return 1 / pow(base, exponent * -1);
 }
 
 
