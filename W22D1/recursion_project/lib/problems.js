d6ff75539ddbd18d48c1bed9c74eae881b5aee2b
Seth Ullman <seth.ullman98@gmail.com>
2020-01-06 16:16:55 -0700

    add finished flatten method

--- W22D1/recursion_project/lib/problems.js
+++ W22D1/recursion_project/lib/problems.js
@@ -119,7 +119,15 @@ function pow(base, exponent) {
 //     2-dimensional array: [['some data']]
 //     3-dimensional array: [[['some data']]]
 function flatten(data) {
+    if (!Array.isArray(data)) return [ data ];
 
+    let newData = [];
+
+    data.forEach( (ele) => {
+        let flattend = flatten(ele);
+        newData.push(...flattend);
+    });
+    return newData;
 }
 
 // Write a function, fileFinder(directories, targetFile), that accepts an object representing directories and a string respresenting a filename.
