986313a95b8758b45526bbb3d5633ac8184eb812
Seth Ullman <seth.ullman98@gmail.com>
2020-01-16 14:19:16 -0700

    add inOrderArray function

---
+++ W23D3/tree_order_project/lib/tree_order.js
@@ -0,0 +1,22 @@
+function inOrderArray(root) {
+    if (!root){
+        return [];
+    }
+
+    return [
+        ...inOrderArray(root.left),
+        root.val,
+        ...inOrderArray(root.right)
+    ];
+
+}
+
+function postOrderArray(root) {
+
+}
+
+
+module.exports = {
+    inOrderArray,
+    postOrderArray
+};
