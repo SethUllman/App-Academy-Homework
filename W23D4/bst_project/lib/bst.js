138e20a7b2838f1fbdcb905c5bb8eb2313fe6258
Seth Ullman <seth.ullman98@gmail.com>
2020-01-17 02:15:57 -0700

    add W23D4 homework

---
+++ W23D4/bst_project/lib/bst.js
@@ -0,0 +1,19 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BST {
+   constructor(){
+       this.root = null;
+   }
+}
+
+module.exports = {
+    TreeNode,
+    BST
+};
